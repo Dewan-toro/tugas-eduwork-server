f5994c1b464c7cc6175bb166d1c8c6747d876ae6
Dewan-toro <dewantoro477@gmail.com>
2024-04-28 17:10:13 +0700

    Membuat API Categories

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
 const productRoute = require("./app/product/router");
+const categoryRoute = require("./app/categories/router");
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", productRoute);
+app.use("/api", categoryRoute);
 //home
 app.use("/", function (req, res, next) {
   res.render("index", { title: "Eduwork API Service" });
