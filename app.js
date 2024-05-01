0ba024f263ba97338a01fa043012a5d9e2b09e0a
Dewan-toro <dewantoro477@gmail.com>
2024-05-01 07:32:44 +0700

    Authentication with PassportJS & JWT

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
+const { decodeToken } = require("./middlewares");
 const productRoute = require("./app/product/router");
 const categoryRoute = require("./app/category/router");
 const tagRoute = require("./app/tag/router");
+const authRoute = require("./app/auth/router");
 
 var app = express();
 
@@ -20,10 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(decodeToken());
 
+app.use("/auth", authRoute);
 app.use("/api", productRoute);
 app.use("/api", categoryRoute);
 app.use("/api", tagRoute);
+
 //home
 app.use("/", function (req, res, next) {
   res.render("index", { title: "Eduwork API Service" });
