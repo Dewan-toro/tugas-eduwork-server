06ac2d27f35d6dd7b26245ea0d78f6a87b31a551
Dewan-toro <dewantoro477@gmail.com>
2024-06-04 13:44:50 +0700

    add products

--- app/tag/router.js
+++ app/tag/router.js
@@ -3,7 +3,7 @@ const { policy_check } = require("../../middlewares");
 const tagController = require("./controller");
 
 router.get("/tags", tagController.index);
-router.post("/tags", policy_check("create", "Tag"), tagController.store);
+router.post("/tags", tagController.store);
 router.put("/tags/:id", policy_check("update", "Tag"), tagController.update);
 router.delete(
   "/tags/:id",
